afe66665812e3adfde39b2c99a8bae8382a3a0c0
Daniel Trolezi <danieltrolezi@outlook.com>
2024-08-30 16:20:00 -0300

    Setup unit testing

---
+++ tests/TestCase.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests;
+
+use PHPUnit\Framework\TestCase as BaseTestCase;
+use Faker\Factory;
+use Faker\Generator;
+
+class TestCase extends BaseTestCase
+{
+    protected Generator $faker;
+
+    public function __construct(?string $name = null, array $data = [], $dataName = '')
+    {
+        parent::__construct($name, $data, $dataName);
+        $this->faker = Factory::create();
+    }
+}
---
+++ tests/Unit/ExampleTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+
+class ExampleTest extends TestCase
+{
+    public function test_should_return_true()
+    {
+        $this->assertTrue(true);
+    }
+}
